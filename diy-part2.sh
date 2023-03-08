47298161bbf29b7cfa85d401af448e7fd11b890d
CoderSpace <d330296190@126.com>
2023-03-08 17:51:55 +0800

    add .config

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,4 +2,4 @@
 #OpenWrt DIY script part 2 (After Update feeds)
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.5/g' package/base-files/files/bin/config_generate
